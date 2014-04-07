aea717b11b2c3b7208dac939334c59ba06931527
AllanDano <allan.dano@hotmail.fr>
2014-04-07 16:44:28 +0200

    Update Level.py

--- src/Level.py
+++ src/Level.py
@@ -1,5 +1,42 @@
+import Settings
+import Coin
+
+
+
+
 def create(setting):
-	#cree le niveau
+	map = mapCreate(setting["levelNumber"])
+	# charge la carte du fichier dans la variable
+	
+	
+	
+	return map
+	
+	
 
 def show(level):
-	#affiche le niveau
+	# affiche le niveau
+	
+	
+	
+	
+	
+def mapCreate(mapNumber):
+	
+	# recupere l'ensemble des cartes
+	fichier = open(maps.txt, "r")
+	chaine = fichier.read()
+	
+	# separation des cartes
+	allMaps = chaine.split("map\n")
+	
+	# test carte demandee existante
+	if mapNumber <= len(allMaps):
+		# separation des lignes de la carte demandee
+		map = allMaps[mapNumber].split("\n")
+	
+
+	
+	
+	
+	
