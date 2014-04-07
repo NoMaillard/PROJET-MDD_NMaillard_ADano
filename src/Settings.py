25cbb60df9c044b6a70e4e3c24490f31edda4e59
Noé Maillard <noemaillard@gmail.com>
2014-04-07 15:48:53 +0200

    Implémentation des fonctions

--- src/Settings.py
+++ src/Settings.py
@@ -1,11 +1,36 @@
+# -*- coding: utf-8 -*-
+
+import sys
+
 def askSesttings():
-	#fait la demande des réglages
+    name = askName()
+    difficulty = askDifficulty()
+    levelNumber = askLevelNumber()
+
+    settings = {"name" : name,"difficulty" : difficulty, "levelNumber" : levelNumber}
+    return settings
 
 def askName():
-	#demande le nom de l'utilisateur
+    name = raw_input("Comment t'appelles-tu ?\n")
+    return name
 
 def askDifficulty():
-	#demande la dificulte
+
+    sys.stdout.write("Choisis ta difficulté\n")
+    sys.stdout.write("1 : Facile\n")
+    sys.stdout.write("2 : Normal\n")
+    sys.stdout.write("3 : Difficile\n")
+    difficulty = raw_input("Difficulté :")
+    difficulty = int(difficulty)
+
+    return difficulty
 
 def askLevelNumber():
-	#demande le niveau a jouer
+
+    levelNumber = raw_input("Quel niveau veux-tu jouer ?")
+    return levelNumber
+
+
+if __name__ == '__main__':
+    settings = askSesttings()
+    print settings
