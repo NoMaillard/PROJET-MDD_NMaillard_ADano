69d862f9e02f1e2e30bb9b9d7c53608a0c9d1b6a
AllanDano <allan.dano@hotmail.fr>
2014-04-14 16:43:12 +0200

    Update Level.py

--- src/Level.py
+++ src/Level.py
@@ -1,68 +1,97 @@
+# -*- coding: utf-8 -*-
 import Settings
 import Coin
 import sys
 import curses
 
 
-win = curses.newwin(20, 80, 0, 0)
 curses.initscr()
+win = curses.newwin(30, 80, 0, 0)
 curses.noecho()
-curses.keypad(1)
+curses.curs_set(0)
+curses.cbreak()
+win.keypad(1)
+win.nodelay(0)
+
+
+
+
 
 
 
 def main():
-        m = create(4)
-        show(m)
+    niveau = dict()
+    niveau = create(niveau, 4)
+    show(niveau)
+    char = win.getch()
 
 
 
+
+
+
+
+def getLevelNumber(level):
+    return level["number"]
+    
+
+
+
+def setLevelNumber(level, nb):
+    level["number"] = nb
+    return level
 
 
 
 
 
 def show(level):
-        assert type(level) is list
-        
-	# affiche le niveau
-        for i in level :
-                ligne = ""
-                for caractere in i :
-                        ligne = ligne + caractere
-                win.addstr(ligne)
-                win.addstr("\n")
-                
-        return
-	
-	
-def create(mapNumber):
-        assert type(mapNumber) is int
-        
-	
-	# recupere l'ensemble des cartes
-        fichier = open('maps.txt', 'r')
-        chaine = fichier.read()
+    assert type(level["map"]) is list
+
+    # affiche le niveau
+    ln = 0
+    for i in level["map"] :
+        ligne = ""
+        for caractere in i :
+            ligne = ligne + caractere
+        win.addstr(ln, 0, ligne)
+        ln += 1
+        win.refresh()
+    return
+
+
+
+
+
+def create(level, mapNumber):
+    assert type(mapNumber) is int
+       
+
+    # recupere l'ensemble des cartes
+    fichier = open('maps.txt', 'r')
+    chaine = fichier.read()
 
         
-	# separation des cartes
-        allMaps = chaine.split("map\n")
-	
-	# test carte demandee existante
-        if mapNumber < len(allMaps):
-                # separation des lignes de la carte demandee
-                level = list(allMaps[mapNumber].split("\n"))
-                return level
-        else:
-                win.addstr("Error\n mapNumber > len(allMaps)\n")
-                return
+    # separation des cartes
+    allMaps = chaine.split("map\n")
+    # test carte demandee existante
+    if mapNumber < len(allMaps):
+        # separation des lignes de la carte demandee
+        level["map"] = list(allMaps[mapNumber].split("\n"))
+        return level
+    else:
+        win.addstr("Error\n mapNumber > len(allMaps)\n")
+    return level
                 
-	
 
 
 if __name__ == "__main__" :
-        main()
+    main()
+    curses.echo()
+    curses.nocbreak()
+    win.keypad(0)
+    curses.endwin()
 
-	
-	
-	
+
+
+
